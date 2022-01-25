b99a5ce88b1b8d25797cf36f32c59678717c03ed
systemshaper <Daniel.Hoover@outlook.com>
2022-01-25 14:55:52 -0600

    4.2 restore initial functionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,8 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
