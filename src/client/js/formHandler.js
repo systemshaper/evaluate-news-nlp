29f9e36eddefb0716f5fe0cac9a09619a4baeccf
systemshaper <Daniel.Hoover@outlook.com>
2022-01-26 19:02:00 -0600

    setup APIs but passing keys still WIP

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,42 @@
+
+
+const getKey = async () => {
+    const fetchedKey = await fetch('http://localhost:8081/key')
+    .then(res => res.json())
+    console.log(fetchedKey)
+    return fetchedKey
+}
+
+const checkIrony = async (text, apiKey) => {
+    const formdata = new FormData();
+    formdata.append("key", apiKey);
+    formdata.append("txt", text);
+    formdata.append("lang", "en");
+    console.log(formdata);
+
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    }
+    
+    fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.irony
+    })
+}
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
 
+    getKey().then(key => checkIrony(formText, key))
+    
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,20 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
+const corsOptions = {
+    origin:'*', 
+    credentials:true,
+    optionSuccessStatus:200,
+}
+dotenv.config()
 const app = express()
 
+const apiKey = process.env.API_KEY
+
 app.use(express.static('dist'))
+app.use(cors(corsOptions))
 
 console.log(__dirname)
 
@@ -13,11 +23,12 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8080!')
+app.get('/key', function(req, res) {
+    res.send({yourKey: apiKey})
+    console.log('sent apiKey ' + apiKey)
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
